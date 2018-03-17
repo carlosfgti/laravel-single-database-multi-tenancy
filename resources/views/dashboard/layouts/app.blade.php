486efbc47a21b101fe6fa1241cbb7d4c5f051127
Carlos Ferreira <conta@especializati.com.br>
2018-03-17 12:08:29 -0300

    Add title dinamic

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Dashboard</title>
+	<title>EspecializaTi - @yield('title')</title>
 
 	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
