dd8ba2e879ba5602c3ef435556ebaf230509c743
Carlos Ferreira <conta@especializati.com.br>
2018-03-17 12:05:35 -0300

    Create Layout Dashboard

--- resources/views/dashboard/home/index.blade.php
+++ resources/views/dashboard/home/index.blade.php
@@ -1,1 +1,8 @@
+@extends('dashboard.home.index')
+
+
+@section('content')
+
 <h1>Dashboard</h1>
+
+@endsection
---
+++ resources/views/dashboard/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Dashboard</title>
+
+	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+	<div class="container">
+		@yield('content')
+	</div>
+
+
+	<script src="{{ mix('css/app.css') }}"></script>
+</body>
+</html>
