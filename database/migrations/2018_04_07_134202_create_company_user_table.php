8d37987a5b973e678d441b1c8b302aeccbedcef6
Carlos Ferreira <conta@especializati.com.br>
2018-04-07 10:49:36 -0300

    Create Table Relationships User <-> Companies

---
+++ database/migrations/2018_04_07_134202_create_company_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanyUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_user', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('company_id')->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_user');
+    }
+}
