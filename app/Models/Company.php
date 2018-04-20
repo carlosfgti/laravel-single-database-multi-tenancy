a862f62d010d6290bff9af8c3a5d6a9003aa672d
Carlos Ferreira <conta@especializati.com.br>
2018-04-20 09:34:59 -0300

    Add  Models

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $fillable = [
+        'name'
+    ];
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
