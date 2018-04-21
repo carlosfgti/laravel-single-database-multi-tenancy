169c72da02ea523eac7add36b8880cb85e1ffc08
Carlos Ferreira <conta@especializati.com.br>
2018-04-21 10:52:02 -0300

    Add Tenant Manager

---
+++ app/Tenant/Manager.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tenant;
+
+class Manager
+{
+	protected $tenant;
+
+
+	public function setTenant($tenant)
+    {
+        $this->tenant = $tenant;
+    }
+
+    public function getTenant()
+    {
+        return $this->tenant;
+    }
+}
