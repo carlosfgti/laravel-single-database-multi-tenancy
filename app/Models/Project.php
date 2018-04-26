3ba8156858577ae6232817a6479f96a068086f0a
Carlos Ferreira <conta@especializati.com.br>
2018-04-26 09:14:25 -0300

    Use Tenant Trait in Model Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Tenant\Traits\TenantsTrait;
 
 class Project extends Model
 {
+	use TenantsTrait;
+
     protected $fillable = [
         'name'
     ];
