e5c038574e86b134c6eb11b58ec6e9815fe12d6f
Carlos Ferreira <conta@especializati.com.br>
2018-05-01 09:24:21 -0300

    Add Controller ProjectController

---
+++ app/Http/Controllers/Tenant/ProjectController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Tenant;
+
+use App\Project;
+use App\Tenant\Manager;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProjectController extends Controller
+{
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required'
+        ]);
+        
+        $request->tenant()->projects()->create([
+            'name' => $request->name
+        ]);
+
+        return back();
+    }
+
+
+    public function show(Project $project)
+    {
+        return view('tenant.projects.show', compact('project'));
+    }
+
+
+    public function update(Request $request, Project $project)
+    {
+        $project->update($request->only('name'));
+
+        return back();
+    }
+}
