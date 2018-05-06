69008873a898e061812018787dbf41bd676a99cf
Carlos Ferreira <conta@especializati.com.br>
2018-05-05 23:31:05 -0300

    Add Middleware in Controller

--- app/Http/Controllers/Tenant/DashboardController.php
+++ app/Http/Controllers/Tenant/DashboardController.php
@@ -8,6 +8,12 @@ use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+
     public function index()
     {
         $projects = Project::get();
